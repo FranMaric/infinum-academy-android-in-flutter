db8f5aac9298a4c73c20d9a54c0d9a2afed14389
Fran Marić <fran.maric.vk@gmail.com>
2021-08-13 19:10:07 +0200

    Renamed folder values -> constants

--- lib/constants/theme_data.dart
+++ lib/constants/theme_data.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 final themeData = ThemeData(
   primaryColor: const Color(0xFF52368C),
   backgroundColor: const Color(0xFF3D1D72),
+  focusColor: const Color(0xFFFFFFFF),
+  hoverColor: const Color(0xFFFFFFFF),
   textTheme: const TextTheme(
     headline2: TextStyle(fontSize: 32.0),
+  ),
+  buttonTheme: const ButtonThemeData(
+    buttonColor: Color(0xff00adb5),
+    disabledColor: Color(0xFFFFFFFF),
   ),
 );
