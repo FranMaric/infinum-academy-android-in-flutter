048dc18c6218008bbda537bb5725cc97583021ba
Fran Marić <fran.maric.vk@gmail.com>
2021-08-19 13:50:52 +0200

    Add ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:infinum_academy_android_flutter/ui/splash_screen/splash_screen.dart';
 import 'constants/theme_data.dart';
 import 'constants/route_generator.dart';
@@ -10,12 +11,14 @@ void main() {
 class ShowsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Shows',
-      theme: themeData,
-      initialRoute: SplashScreen.routeName,
-      onGenerateRoute: RouteGenerator.generateRoute,
+    return ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Shows',
+        theme: themeData,
+        initialRoute: SplashScreen.routeName,
+        onGenerateRoute: RouteGenerator.generateRoute,
+      ),
     );
   }
 }
