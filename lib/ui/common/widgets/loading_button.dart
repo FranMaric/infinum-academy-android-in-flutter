dd32b5a6b53de43c59983fb402fb47bc1c14b590
Fran Marić <fran.maric.vk@gmail.com>
2021-08-19 17:07:27 +0200

    Create custom button

---
+++ lib/ui/common/widgets/loading_button.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum ButtonState { enabled, disabled, loading }
+
+final defaultButtonStateProvider =
+    StateProvider<ButtonState>((ref) => ButtonState.enabled);
+
+class LoadingButton extends ConsumerWidget {
+  const LoadingButton({
+    Key? key,
+    required this.onPressed,
+    this.title = 'null',
+    this.buttonStateProvider,
+    this.disabledBackgroundColor = const Color(0xffbbbbbb),
+    this.enabledBackgroundColor = const Color(0xFFFFFFFF),
+    this.disabledTitleColor = const Color(0xFFFFFFFF),
+    this.enabledTitleColor = const Color(0xFF52368C),
+    this.margin,
+  }) : super(key: key);
+
+  final StateProvider<ButtonState>? buttonStateProvider;
+
+  final void Function()? onPressed;
+  final String title;
+
+  final Color disabledBackgroundColor;
+  final Color enabledBackgroundColor;
+  final Color disabledTitleColor;
+  final Color enabledTitleColor;
+
+  final EdgeInsets? margin;
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final ButtonState buttonState =
+        watch(buttonStateProvider ?? defaultButtonStateProvider).state;
+
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: buttonState == ButtonState.enabled
+            ? enabledBackgroundColor
+            : disabledBackgroundColor,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      margin: margin,
+      child: MaterialButton(
+        onPressed: buttonState == ButtonState.enabled ? onPressed : null,
+        child: buttonState == ButtonState.loading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Text(
+                title,
+                style: Theme.of(context).textTheme.button?.copyWith(
+                      color: buttonState == ButtonState.enabled
+                          ? enabledTitleColor
+                          : disabledTitleColor,
+                    ),
+              ),
+      ),
+    );
+  }
+}
