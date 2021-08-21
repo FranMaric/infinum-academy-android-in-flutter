beb7c05591db1bb399a74d5f7642560391396bed
Fran Marić <fran.maric.vk@gmail.com>
2021-08-21 14:52:35 +0200

    Working ApiClient for login and register

--- lib/services/api/api_client.dart
+++ lib/services/api/api_client.dart
@@ -1,3 +1,4 @@
+import 'package:alice/alice.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -7,7 +8,7 @@ final apiClientProvider = Provider((ref) => ApiClient());
 class ApiClient {
   late Dio _dio;
 
-  void init() {
+  void init(Alice alice) {
     _dio = Dio(
       BaseOptions(
         baseUrl: 'https://tv-shows.infinum.academy/',
@@ -15,19 +16,49 @@ class ApiClient {
         receiveTimeout: 6000,
       ),
     );
-    _dio.interceptors.add(InterceptorsWrapper(
-      onRequest: (options, handler) async {
-        final prefs = await SharedPreferences.getInstance();
+    _dio.interceptors.addAll(
+      [
+        InterceptorsWrapper(
+          onRequest: (options, handler) async {
+            if (options.path != '/users' && options.path != '/users/sign_in') {
+              final prefs = await SharedPreferences.getInstance();
 
-        options.headers.addAll(
-          <String, String>{
-            'access-token': prefs.getString('access-token') ?? '',
-            'client': prefs.getString('client') ?? '',
-            'uid': prefs.getString('uid') ?? '',
-            'token-type': 'Bearer',
+              options.headers.addAll(
+                <String, String>{
+                  'access-token': prefs.getString('access-token') ?? '',
+                  'client': prefs.getString('client') ?? '',
+                  'uid': prefs.getString('uid') ?? '',
+                  'token-type': 'Bearer',
+                },
+              );
+            }
+
+            return handler.next(options);
           },
-        );
+        ),
+        alice.getDioInterceptor()
+      ],
+    );
+  }
+
+  Future<Response<dynamic>> register(String email, String password, String confirmationPassword) async {
+    return _dio.post(
+      '/users',
+      data: {
+        "email": email,
+        "password": password,
+        "password_confirmation": confirmationPassword,
       },
-    ));
+    );
+  }
+
+  Future<Response<dynamic>> login(String email, String password) async {
+    return _dio.post(
+      '/users/sign_in',
+      data: {
+        "email": email,
+        "password": password,
+      },
+    );
   }
 }
