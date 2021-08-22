4055db337382f1d18d5132ab8cab86d20a36e95b
Fran Marić <fran.maric.vk@gmail.com>
2021-08-22 14:49:11 +0200

    Add showsRepositoryProvider initialization

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:infinum_academy_android_flutter/services/api/api_client.dart';
 import 'package:infinum_academy_android_flutter/services/authentication/authentication_client.dart';
+import 'package:infinum_academy_android_flutter/services/shows_repository/shows_repository.dart';
 import 'package:infinum_academy_android_flutter/ui/splash_screen/splash_screen.dart';
 import 'constants/theme_data.dart';
 import 'constants/route_generator.dart';
@@ -18,6 +19,7 @@ class ShowsApp extends StatelessWidget {
 
     context.read(apiClientProvider).init(alice);
     context.read(authProvider).apiClient = context.read(apiClientProvider);
+    context.read(showsRepositoryProvider).apiClient = context.read(apiClientProvider);
 
     return MaterialApp(
       navigatorKey: alice.getNavigatorKey(),
