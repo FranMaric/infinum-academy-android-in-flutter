1b01f5747e161b1164f868e4d0e2f7c24079068a
Fran Marić <fran.maric.vk@gmail.com>
2021-08-22 19:43:38 +0200

    Top rated shows functionality working

--- lib/ui/shows_screen/shows_screen.dart
+++ lib/ui/shows_screen/shows_screen.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:infinum_academy_android_flutter/services/shows_repository/shows_repository.dart';
+import 'package:infinum_academy_android_flutter/ui/shows_screen/widgets/shows_list.dart';
+import 'package:infinum_academy_android_flutter/ui/shows_screen/widgets/top_rated_chip.dart';
+
+final _showsFutureProvider = FutureProvider.autoDispose((ref) async {
+  ref.maintainState = true;
+
+  final showsRepository = ref.read(showsRepositoryProvider);
+  final shows = await showsRepository.getShows(isTopRated: ref.read(_isTopRatedProvider).state);
+
+  return shows;
+});
+
+final _isTopRatedProvider = StateProvider((ref) => false);
 
 class ShowsScreen extends StatelessWidget {
   const ShowsScreen({Key? key}) : super(key: key);
@@ -9,12 +24,32 @@ class ShowsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Shows'),
-        toolbarHeight: 100.0,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(75.0),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Shows',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                TopRatedChip(onPressed: (isTopRated) {
+                  context.read(_isTopRatedProvider).state = isTopRated;
+                  context.refresh(_showsFutureProvider);
+                })
+              ],
+            ),
+          ),
+        ),
+        toolbarHeight: 150.0,
+        elevation: 0.0,
+        backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+        textTheme: Theme.of(context).textTheme,
+        titleTextStyle: Theme.of(context).textTheme.headline6,
       ),
-      body: const Center(
-        child: Text('Show screen'),
-      ),
+      body: ShowsList(showsFutureProvider: _showsFutureProvider),
     );
   }
 }
