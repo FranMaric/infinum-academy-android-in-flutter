3bac0f58dd8ec4272d4a4b4b878afcf6ab353768
Fran Marić <fran.maric.vk@gmail.com>
2021-08-22 14:38:52 +0200

    Create centered_circular_progress_indicator.dart

---
+++ lib/ui/common/widgets/centered_circular_progress_indicator.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CenteredCircularProgressIndicator extends StatelessWidget {
+  const CenteredCircularProgressIndicator({
+    Key? key,
+    this.value,
+    this.backgroundColor,
+    this.color,
+    this.valueColor,
+    this.strokeWidth = 4.0,
+    this.semanticsLabel,
+    this.semanticsValue,
+  }) : super(key: key);
+
+  final Color? backgroundColor;
+  final Color? color;
+  final Animation<Color?>? valueColor;
+  final String? semanticsLabel;
+  final String? semanticsValue;
+  final double strokeWidth;
+  final double? value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(
+        backgroundColor: backgroundColor,
+        color: color,
+        valueColor: valueColor,
+        semanticsLabel: semanticsLabel,
+        semanticsValue: semanticsValue,
+        strokeWidth: strokeWidth,
+        value: value,
+      ),
+    );
+  }
+}
