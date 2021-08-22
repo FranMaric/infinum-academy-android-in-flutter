a69705d35d37d92efbe3af63b91e195324211b12
Fran Marić <fran.maric.vk@gmail.com>
2021-08-22 19:43:07 +0200

    Create top_rated_chip.dart

---
+++ lib/ui/shows_screen/widgets/top_rated_chip.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class TopRatedChip extends StatefulWidget {
+  const TopRatedChip({Key? key, required this.onPressed}) : super(key: key);
+
+  final void Function(bool) onPressed;
+
+  @override
+  _TopRatedChipState createState() => _TopRatedChipState();
+}
+
+class _TopRatedChipState extends State<TopRatedChip> {
+  late bool _isChecked;
+
+  @override
+  void initState() {
+    super.initState();
+    _isChecked = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ActionChip(
+      label: Text(
+        'Top rated',
+        style: Theme.of(context).textTheme.bodyText2?.copyWith(
+              color: _isChecked ? Colors.white : Theme.of(context).primaryColor,
+            ),
+      ),
+      backgroundColor: _isChecked ? Theme.of(context).primaryColor : Colors.white,
+      shape: StadiumBorder(
+        side: BorderSide(color: Theme.of(context).primaryColor),
+      ),
+      avatar: SvgPicture.asset(
+        'assets/full_heart_icon.svg',
+        color: _isChecked ? Colors.white : Theme.of(context).primaryColor,
+      ),
+      onPressed: () {
+        setState(() {
+          _isChecked = !_isChecked;
+          widget.onPressed(_isChecked);
+        });
+      },
+    );
+  }
+}
