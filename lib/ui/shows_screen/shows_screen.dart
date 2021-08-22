2fedf832d970cf640250886b2479f9018f76361b
Fran Marić <fran.maric.vk@gmail.com>
2021-08-22 20:17:07 +0200

    Completed AppBar

--- lib/ui/shows_screen/shows_screen.dart
+++ lib/ui/shows_screen/shows_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:infinum_academy_android_flutter/constants/shared_prefs_keys.dart';
 import 'package:infinum_academy_android_flutter/services/shows_repository/shows_repository.dart';
 import 'package:infinum_academy_android_flutter/ui/shows_screen/widgets/shows_list.dart';
 import 'package:infinum_academy_android_flutter/ui/shows_screen/widgets/top_rated_chip.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final _showsFutureProvider = FutureProvider.autoDispose((ref) async {
   ref.maintainState = true;
@@ -20,10 +22,40 @@ class ShowsScreen extends StatelessWidget {
 
   static const routeName = '/shows';
 
+  void showProfileBottomSheet() {
+    // TODO: show profile bottom sheet
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          GestureDetector(
+            onTap: showProfileBottomSheet,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 25.0, horizontal: 20.0),
+              child: ClipOval(
+                child: FutureBuilder<SharedPreferences>(
+                    future: SharedPreferences.getInstance(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData &&
+                          snapshot.data != null &&
+                          snapshot.data?.getString(prefsProfilePhotoUrlKey) != null &&
+                          snapshot.data?.getString(prefsProfilePhotoUrlKey) != 'null') {
+                        return Image.network(snapshot.data!.getString(prefsProfilePhotoUrlKey)!);
+                      }
+                      return Image.asset(
+                        'assets/avatar_placeholder_icon.png',
+                        fit: BoxFit.fill,
+                        width: 24.0,
+                        height: 24.0,
+                      );
+                    }),
+              ),
+            ),
+          ),
+        ],
         bottom: PreferredSize(
           preferredSize: const Size.fromHeight(75.0),
           child: Padding(
