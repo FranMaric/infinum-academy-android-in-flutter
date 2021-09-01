59e9000c7e3ceff280045e086735d65f92c4e322
Fran Marić <fran.maric.vk@gmail.com>
2021-09-02 00:24:40 +0200

    Add state management inside ProfilePhoto widget

--- lib/ui/shows_screen/widgets/profile_photo.dart
+++ lib/ui/shows_screen/widgets/profile_photo.dart
@@ -1,8 +1,29 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:infinum_academy_android_flutter/constants/shared_prefs_keys.dart';
+import 'package:infinum_academy_android_flutter/services/shows_exception.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class ProfilePhoto extends StatelessWidget {
+const placeHolderImageProvider = AssetImage('assets/avatar_placeholder_icon.png');
+
+final profilePhotoFutureProvider = FutureProvider<ImageProvider>((ref) async {
+  final prefs = await SharedPreferences.getInstance();
+  final url = prefs.getString(prefsProfilePhotoUrlKey);
+
+  if (url == null || url.trim().isEmpty) {
+    throw ShowsException('Empty image url');
+  }
+
+  if (url.startsWith('http')) {
+    return NetworkImage(url);
+  }
+
+  return FileImage(File(url));
+});
+
+class ProfilePhoto extends ConsumerWidget {
   const ProfilePhoto({
     Key? key,
     this.width = 24.0,
@@ -18,36 +39,25 @@ class ProfilePhoto extends StatelessWidget {
   final Color borderColor;
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<SharedPreferences>(
-        future: SharedPreferences.getInstance(),
-        builder: (context, snapshot) {
-          final isAvailableNetworkImage = snapshot.hasData &&
-              snapshot.data != null &&
-              snapshot.data?.getString(prefsProfilePhotoUrlKey) != null &&
-              snapshot.data?.getString(prefsProfilePhotoUrlKey) != 'null';
-
-          return Container(
-            width: width,
-            height: height,
-            decoration: BoxDecoration(
-              color: const Color(0xff7c94b6),
-              image: isAvailableNetworkImage
-                  ? DecorationImage(
-                      image: NetworkImage(snapshot.data!.getString(prefsProfilePhotoUrlKey)!),
-                      fit: BoxFit.cover,
-                    )
-                  : const DecorationImage(
-                      image: AssetImage('assets/avatar_placeholder_icon.png'),
-                      fit: BoxFit.cover,
-                    ),
-              borderRadius: const BorderRadius.all(Radius.circular(50.0)),
-              border: Border.all(
-                color: borderColor,
-                width: borderWidth,
-              ),
-            ),
-          );
-        });
+  Widget build(BuildContext context, ScopedReader watch) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: watch(profilePhotoFutureProvider).when(
+            data: (imageProvider) => imageProvider,
+            loading: () => placeHolderImageProvider,
+            error: (e, st) => placeHolderImageProvider,
+          ),
+          fit: BoxFit.cover,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(50.0)),
+        border: Border.all(
+          color: borderColor,
+          width: borderWidth,
+        ),
+      ),
+    );
   }
 }
