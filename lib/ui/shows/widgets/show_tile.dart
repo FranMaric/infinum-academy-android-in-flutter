8eea2f82eeb5a3eb21fb25504ba4d66f5453852e
Fran Marić <fran.maric.vk@gmail.com>
2021-09-05 17:14:59 +0200

    Switching grid/colun layout working

--- lib/ui/shows/widgets/show_tile.dart
+++ lib/ui/shows/widgets/show_tile.dart
@@ -3,7 +3,6 @@ import 'package:infinum_academy_android_flutter/models/show.dart';
 import 'package:infinum_academy_android_flutter/ui/shows/widgets/show_image.dart';
 
 const elevation = 5.0;
-const margin = 15.0;
 const borderRadius = 8.0;
 
 class ShowTile extends StatelessWidget {
@@ -21,42 +20,47 @@ class ShowTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
+      padding: EdgeInsets.zero,
       onPressed: onPressed,
-      child: Container(
-        margin: const EdgeInsets.all(margin),
-        child: Material(
-          elevation: elevation,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
-          ),
-          clipBehavior: Clip.antiAlias,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (show.imageUrl != null) ShowImage(url: show.imageUrl!),
+      child: Material(
+        elevation: elevation,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (show.imageUrl != null) ShowImage(url: show.imageUrl!),
+            Padding(
+              padding: showDescription
+                  ? const EdgeInsets.only(
+                      top: 16.0,
+                      left: 24.0,
+                    )
+                  : const EdgeInsets.only(
+                      left: 16,
+                      top: 16,
+                      right: 16,
+                      bottom: 40,
+                    ),
+              child: Text(
+                show.title,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+            ),
+            if (show.description != null && showDescription)
               Padding(
-                padding: const EdgeInsets.only(
-                  top: 16.0,
-                  left: 24.0,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24.0,
+                  vertical: 14.0,
                 ),
                 child: Text(
-                  show.title,
-                  style: Theme.of(context).textTheme.subtitle1,
+                  show.description!,
+                  style: Theme.of(context).textTheme.bodyText2,
                 ),
               ),
-              if (show.description != null && showDescription)
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 24.0,
-                    vertical: 14.0,
-                  ),
-                  child: Text(
-                    show.description!,
-                    style: Theme.of(context).textTheme.bodyText2,
-                  ),
-                ),
-            ],
-          ),
+          ],
         ),
       ),
     );
--- lib/ui/shows/widgets/shows_list.dart
+++ lib/ui/shows/widgets/shows_list.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:infinum_academy_android_flutter/models/show.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:infinum_academy_android_flutter/services/shows_exception.dart';
 import 'package:infinum_academy_android_flutter/ui/common/widgets/centered_circular_progress_indicator.dart';
 import 'package:infinum_academy_android_flutter/ui/show_details/show_details_screen.dart';
+import 'package:infinum_academy_android_flutter/ui/shows/shows_screen.dart';
 import 'package:infinum_academy_android_flutter/ui/shows/widgets/no_shows_widget.dart';
 import 'package:infinum_academy_android_flutter/ui/shows/widgets/show_tile.dart';
 
@@ -14,34 +15,46 @@ class ShowsList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ScopedReader watch) {
+    final isColumnLayout = watch(showsLayoutProvider).state == ShowsLayout.column;
+
     return RefreshIndicator(
       onRefresh: () => context.refresh(showsFutureProvider),
       color: Theme.of(context).primaryColor,
       strokeWidth: 2.5,
-      child: watch(showsFutureProvider).when(
-          loading: () => const CenteredCircularProgressIndicator(),
-          error: (err, stack) {
-            if (err is ShowsException) {
-              return Center(
-                child: Text(err.message),
-              );
-            }
-            return const Text('Oops, something went wrong');
-          },
-          data: (shows) {
-            if (shows.isEmpty) {
-              return const NoShowsWidget();
-            }
-            return ListView.builder(
-              itemCount: shows.length,
-              itemBuilder: (context, index) {
-                return ShowTile(
-                  show: shows[index],
-                  onTap: () => Navigator.of(context).pushNamed(ShowDetailsScreen.routeName, arguments: shows[index]),
+      child: Container(
+        color: Colors.white,
+        child: watch(showsFutureProvider).when(
+            loading: () => const CenteredCircularProgressIndicator(),
+            error: (err, stack) {
+              if (err is ShowsException) {
+                return Center(
+                  child: Text(err.message),
                 );
-              },
-            );
-          }),
+              }
+              return const Text('Oops, something went wrong');
+            },
+            data: (shows) {
+              if (shows.isEmpty) {
+                return const NoShowsWidget();
+              }
+
+              return StaggeredGridView.countBuilder(
+                crossAxisCount: isColumnLayout ? 1 : 2,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24.0,
+                ),
+                crossAxisSpacing: 16.0,
+                mainAxisSpacing: 16.0,
+                itemCount: shows.length,
+                itemBuilder: (BuildContext context, int index) => ShowTile(
+                  show: shows[index],
+                  showDescription: isColumnLayout,
+                  onPressed: () => Navigator.of(context).pushNamed(ShowDetailsScreen.routeName, arguments: shows[index]),
+                ),
+                staggeredTileBuilder: (int index) => const StaggeredTile.fit(1),
+              );
+            }),
+      ),
     );
   }
 }
