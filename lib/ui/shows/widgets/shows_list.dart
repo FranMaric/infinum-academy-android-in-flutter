4b06c146ef8c0ae7aca6da3476dc32c5717e554c
Fran Marić <fran.maric.vk@gmail.com>
2021-09-05 17:57:18 +0200

    FIX: ShowsList working perfectly

--- lib/ui/shows/widgets/shows_list.dart
+++ lib/ui/shows/widgets/shows_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:infinum_academy_android_flutter/models/show.dart';
 import 'package:infinum_academy_android_flutter/services/shows_exception.dart';
 import 'package:infinum_academy_android_flutter/ui/common/widgets/centered_circular_progress_indicator.dart';
 import 'package:infinum_academy_android_flutter/ui/show_details/show_details_screen.dart';
@@ -24,36 +25,53 @@ class ShowsList extends ConsumerWidget {
       child: Container(
         color: Colors.white,
         child: watch(showsFutureProvider).when(
-            loading: () => const CenteredCircularProgressIndicator(),
-            error: (err, stack) {
-              if (err is ShowsException) {
-                return Center(
-                  child: Text(err.message),
-                );
-              }
-              return const Text('Oops, something went wrong');
-            },
-            data: (shows) {
-              if (shows.isEmpty) {
-                return const NoShowsWidget();
-              }
+          loading: () => const CenteredCircularProgressIndicator(),
+          error: (err, stack) {
+            if (err is ShowsException) {
+              return Center(
+                child: Text(err.message),
+              );
+            }
+            return const Text('Oops, something went wrong');
+          },
+          data: (shows) {
+            if (shows.isEmpty) {
+              return const NoShowsWidget();
+            }
 
-              return StaggeredGridView.countBuilder(
-                crossAxisCount: isColumnLayout ? 1 : 2,
+            if (isColumnLayout) {
+              return ListView.builder(
                 padding: const EdgeInsets.symmetric(
                   horizontal: 24.0,
                 ),
-                crossAxisSpacing: 16.0,
-                mainAxisSpacing: 16.0,
                 itemCount: shows.length,
-                itemBuilder: (BuildContext context, int index) => ShowTile(
-                  show: shows[index],
-                  showDescription: isColumnLayout,
-                  onPressed: () => Navigator.of(context).pushNamed(ShowDetailsScreen.routeName, arguments: shows[index]),
+                itemBuilder: (BuildContext context, int index) => Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: ShowTile(
+                    show: shows[index],
+                    onPressed: () => Navigator.of(context).pushNamed(ShowDetailsScreen.routeName, arguments: shows[index]),
+                  ),
                 ),
-                staggeredTileBuilder: (int index) => const StaggeredTile.fit(1),
               );
-            }),
+            }
+
+            return StaggeredGridView.countBuilder(
+              crossAxisCount: 2,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24.0,
+              ),
+              crossAxisSpacing: 16.0,
+              mainAxisSpacing: 16.0,
+              itemCount: shows.length,
+              itemBuilder: (BuildContext context, int index) => ShowTile(
+                show: shows[index],
+                showDescription: false,
+                onPressed: () => Navigator.of(context).pushNamed(ShowDetailsScreen.routeName, arguments: shows[index]),
+              ),
+              staggeredTileBuilder: (int index) => const StaggeredTile.fit(1),
+            );
+          },
+        ),
       ),
     );
   }
