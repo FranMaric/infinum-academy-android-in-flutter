f3bcc87dea7ad735078db3a44e1a8b46149f8e59
Fran Marić <fran.maric.vk@gmail.com>
2021-09-21 15:04:17 +0200

    Custom rating bar completed

--- lib/ui/show_details/widgets/rating_bar.dart
+++ lib/ui/show_details/widgets/rating_bar.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-
-const fullStarAsset = 'assets/images/full_star_icon.svg';
-const emptyStarAsset = 'assets/images/empty_star_icon.svg';
 
 class RatingBar extends StatefulWidget {
   const RatingBar({
     Key? key,
     this.initialRating = 0,
     this.itemCount = 5,
+    this.itemSize = 40.0,
     this.ratingController,
+    this.lineWidth = 3.0,
+    this.itemPadding = const EdgeInsets.symmetric(horizontal: 4.0),
+    this.ignoreGestures = false,
   }) : super(key: key);
 
-  final int initialRating;
+  final double initialRating;
+  final double itemSize;
+  final double lineWidth;
+  final EdgeInsets itemPadding;
   final int itemCount;
   final RatingController? ratingController;
+  final bool ignoreGestures;
 
   @override
   _RatingBarState createState() => _RatingBarState();
@@ -26,62 +30,133 @@ class _RatingBarState extends State<RatingBar> {
   @override
   void initState() {
     super.initState();
-    if (widget.ratingController != null) {
-      ratingController = widget.ratingController!;
-      ratingController.addListener(
-        () => setState(() {}),
-      );
-    } else {
-      ratingController = RatingController(rating: widget.initialRating);
-    }
+    _setupRatingController();
+  }
+
+  void _setupRatingController() {
+    ratingController = widget.ratingController ?? RatingController(widget.initialRating);
+    ratingController.addListener(
+      () => setState(() {}),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisSize: MainAxisSize.min,
       children: List.generate(
         widget.itemCount,
-        (index) => GestureDetector(
-          onTap: () {
-            setState(() {
-              ratingController.rating = index + 1;
-            });
-          },
-          child: index < ratingController.rating ? Star.full() : Star.empty(),
-        ),
+        (index) {
+          final double value;
+          if (index < ratingController.rating.floorToDouble()) {
+            value = 1;
+          } else if (index < ratingController.rating) {
+            value = ratingController.rating - ratingController.rating.floorToDouble();
+          } else {
+            value = 0;
+          }
+
+          return Padding(
+            padding: widget.itemPadding,
+            child: GestureDetector(
+              onTap: () {
+                if (widget.ignoreGestures) return;
+                ratingController.rating = index + 1;
+              },
+              onHorizontalDragDown: (details) {
+                if (widget.ignoreGestures) return;
+                ratingController.rating = index + 1;
+              },
+              child: CustomPaint(
+                size: Size(widget.itemSize, widget.itemSize),
+                painter: StarPainter(
+                  color: Theme.of(context).primaryColor,
+                  lineWidth: widget.lineWidth,
+                  value: value,
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 }
 
-class Star extends StatelessWidget {
-  Star.full({Key? key}) : super(key: key) {
-    starAsset = fullStarAsset;
-  }
+class RatingController extends ValueNotifier<double> {
+  RatingController([double? rating]) : super(rating ?? 0);
 
-  Star.empty({Key? key}) : super(key: key) {
-    starAsset = emptyStarAsset;
-  }
+  double get rating => value;
 
-  late final String starAsset;
-
-  @override
-  Widget build(BuildContext context) {
-    return SvgPicture.asset(
-      starAsset,
-      width: 40,
-      height: 38,
-      color: Theme.of(context).primaryColor,
-    );
+  set rating(double newValue) {
+    value = newValue;
   }
 }
 
-class RatingController extends ValueNotifier<int> {
-  RatingController({int? rating}) : super(rating ?? 0);
+class StarPainter extends CustomPainter {
+  StarPainter({
+    required this.color,
+    required this.value,
+    required this.lineWidth,
+  }) : assert(value >= 0 && value <= 1, 'Value must be between 0 and 1 inclusive');
 
-  int get rating => value;
+  final Color color;
 
-  set rating(int newValue) {
-    value = newValue;
+  /// double between 0 and 1 inclusive
+  final double value;
+
+  final double lineWidth;
+
+  /// Points of a star in a unit grid
+  /// Starting at top point and going clockwise
+  static const List<Offset> _points = [
+    Offset(0.50, 0.00),
+    Offset(0.60, 0.37),
+    Offset(0.97, 0.37),
+    Offset(0.68, 0.60),
+    Offset(0.79, 0.97),
+    Offset(0.50, 0.75),
+    Offset(0.21, 0.97),
+    Offset(0.32, 0.60),
+    Offset(0.03, 0.37),
+    Offset(0.40, 0.37),
+  ];
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final points = _points.map((offset) => offset.scale(size.width, size.height - lineWidth)).toList();
+
+    final paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = lineWidth;
+
+    final path = Path()..addPolygon(points, true);
+    canvas.drawPath(path, paint);
+
+    if (value == 1) {
+      paint.style = PaintingStyle.fill;
+      canvas.drawPath(path, paint);
+    } else if (value > 0.25 && value < 0.75) {
+      final leftHalf = Path()..addPolygon(points.sublist(5)..add(points.first), true);
+
+      paint.style = PaintingStyle.fill;
+      canvas.drawPath(leftHalf, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(StarPainter oldDelegate) {
+    if (color != oldDelegate.color) {
+      return true;
+    }
+    if (value < 0.25 && oldDelegate.value < 0.25 || // if they are both empty stars no need to repaint
+        value > 0.25 && value < 0.75 && oldDelegate.value > 0.25 && oldDelegate.value < 0.75 || // if they are both half stars no need to repaint
+        value > 0.75 && oldDelegate.value > 0.75) // if they are both full stars no need to repaint
+    {
+      return false;
+    }
+
+    return true;
   }
 }
