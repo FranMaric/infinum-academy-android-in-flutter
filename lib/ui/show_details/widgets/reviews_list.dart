34df86e2d1b2a1b75c99d367491b4ce8c9dac6d2
Fran Marić <fran.maric.vk@gmail.com>
2021-09-21 22:00:20 +0200

    Converted state management from FutureProvider to ReviewsNotifierProvider

--- lib/ui/show_details/widgets/reviews_list.dart
+++ lib/ui/show_details/widgets/reviews_list.dart
@@ -1,51 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:infinum_academy_android_flutter/models/review.dart';
-import 'package:infinum_academy_android_flutter/services/shows_exception.dart';
-import 'package:infinum_academy_android_flutter/services/shows_repository.dart';
-import 'package:infinum_academy_android_flutter/ui/common/widgets/centered_circular_progress_indicator.dart';
+import 'package:infinum_academy_android_flutter/ui/show_details/show_details_screen.dart';
 import 'package:infinum_academy_android_flutter/ui/show_details/widgets/review_tile.dart';
-
-final reviewsFamily = FutureProvider.family<List<Review>, int>((ref, showId) async {
-  final showsRepository = ref.watch(showsRepositoryProvider);
-  final reviews = await showsRepository.getReviews(showId: showId);
-
-  return reviews;
-});
 
 class ReviewsList extends ConsumerWidget {
   const ReviewsList({
     Key? key,
-    required this.showId,
   }) : super(key: key);
-
-  final int showId;
 
   @override
   Widget build(BuildContext context, ScopedReader watch) {
-    return watch(reviewsFamily(showId)).when(
-        data: (reviews) {
-          if (reviews.isEmpty) {
-            return const Text('No reviews yet.');
-          }
+    final reviews = watch(reviewsNotifierProvider).reviews;
 
-          return ListView.builder(
-            itemCount: reviews.length,
-            primary: false,
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) => ReviewTile(
-              reviews[index],
-              hasBottomDivider: index != reviews.length - 1,
-            ),
-          );
-        },
-        loading: () => const CenteredCircularProgressIndicator(),
-        error: (err, stack) {
-          if (err is ShowsException) {
-            return Text(err.message);
-          }
-          return const Text('Something went wrong');
-        });
+    if (reviews.isEmpty) {
+      return const Center(
+        child: Text(
+          'No reviews yet.',
+        ),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: reviews.length,
+      primary: false,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) => ReviewTile(
+        reviews[index],
+        hasBottomDivider: index != reviews.length - 1,
+      ),
+    );
   }
 }
