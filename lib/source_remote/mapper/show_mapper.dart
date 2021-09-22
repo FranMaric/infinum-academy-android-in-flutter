e6ad0ac5da4e0ad0cf1c4e71b862afab053601cf
Fran Marić <fran.maric.vk@gmail.com>
2021-09-22 19:33:19 +0200

    Add mappers for converting DBShow to Show and vice versa

---
+++ lib/source_remote/mapper/show_mapper.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: avoid_classes_with_only_static_members
+
+import 'package:infinum_academy_android_flutter/common/models/show.dart';
+import 'package:infinum_academy_android_flutter/source_local/database/shows_database.dart';
+
+class ShowMapper {
+  static Show mapFromDBShow(DBShow show) {
+    return Show(
+      id: show.id.toString(),
+      noOfReviews: show.noOfReviews,
+      title: show.title,
+      averageRating: show.averageRating,
+      description: show.description,
+      imageUrl: show.imageUrl,
+    );
+  }
+}
