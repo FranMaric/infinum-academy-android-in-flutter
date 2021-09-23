e1632fb432ae24fd8bfc827fe68b7d6aaffbc286
Fran Marić <fran.maric.vk@gmail.com>
2021-09-23 13:50:15 +0200

    Add storing and getting Shows from database

--- lib/domain/shows_repository.dart
+++ lib/domain/shows_repository.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:infinum_academy_android_flutter/source_local/mapper/db_show_mapper.dart';
 import 'package:infinum_academy_android_flutter/source_local/shared_prefs_keys.dart';
 import 'package:infinum_academy_android_flutter/common/models/new_review.dart';
 import 'package:infinum_academy_android_flutter/common/models/review.dart' show Review;
@@ -10,7 +11,8 @@ import 'package:infinum_academy_android_flutter/common/models/user.dart';
 import 'package:infinum_academy_android_flutter/source_remote/api_client.dart';
 import 'package:infinum_academy_android_flutter/domain/shows_exception.dart';
 import 'package:infinum_academy_android_flutter/extensions/nullable_int_extension.dart';
-import 'package:infinum_academy_android_flutter/source_local/database/shows_database.dart' show ShowsDatabase;
+import 'package:infinum_academy_android_flutter/source_local/database/shows_database.dart';
+import 'package:infinum_academy_android_flutter/source_remote/mapper/show_mapper.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -21,26 +23,53 @@ final showsRepositoryProvider = Provider((ref) => ShowsRepository());
 /// Encapsulates [ApiClient] and [ShowsDatabase] for easy offline/online handling
 class ShowsRepository {
   late final ApiClient _apiClient;
+  late final ShowsDatabase _database;
 
   /// Call [init] as soon as possible
-  Future<void> init(ApiClient apiClient) async {
+  Future<void> init(ApiClient apiClient, ShowsDatabase database) async {
     _apiClient = apiClient;
+    _database = database;
     _checkForOfflinePhotoUpload();
   }
 
   Future<List<Show>> getShows({required bool isTopRated}) async {
     try {
+      final hasInternetConnection = await _apiClient.hasInternetConnection();
+
+      if (!hasInternetConnection) {
+        if (isTopRated) {
+          return [];
+        }
+
+        return _getShowsFromDB();
+      }
+
       final response = isTopRated ? await _apiClient.getTopRatedShows() : await _apiClient.getShows();
 
       if (response.statusCode.isSuccessful) {
         final shows = List<Map<String, dynamic>>.from(response.data['shows'] as List).map((show) => Show.fromJson(show)).toList();
+
+        if (!isTopRated) {
+          _addShowsToDB(shows);
+        }
 
         return shows;
       }
     } on DioError catch (dioError) {
       throw ShowsException.fromDioError(dioError);
     }
-    return [];
+
+    return _getShowsFromDB();
+  }
+
+  Future<List<Show>> _getShowsFromDB() async {
+    final dbShows = await _database.showDao.getAllShows();
+
+    return dbShows.map((dbShow) => ShowMapper.mapFromDBShow(dbShow)).toList();
+  }
+
+  Future<void> _addShowsToDB(List<Show> shows) async {
+    _database.showDao.addShows(shows.map((show) => DBShowMapper.mapFromShow(show)).toList());
   }
 
   Future<List<Review>> getReviews({required int showId}) async {
