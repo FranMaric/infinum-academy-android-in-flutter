de7d9a1a211ad50ce05f0884ae32ba91bfdf62d1
Fran Marić <fran.maric.vk@gmail.com>
2021-09-28 20:55:14 +0200

    Add shared preferences initialization

--- lib/app/shows_app.dart
+++ lib/app/shows_app.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:infinum_academy_android_flutter/constants/route_generator.dart';
 import 'package:infinum_academy_android_flutter/constants/theme_data.dart';
+import 'package:infinum_academy_android_flutter/source_local/shared_preferences/shared_preferences_provider.dart';
 import 'package:infinum_academy_android_flutter/source_remote/api_client.dart';
 import 'package:infinum_academy_android_flutter/ui/splash/splash_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void runShowsApp() {
-  runApp(ProviderScope(child: ShowsApp()));
+Future<void> runShowsApp() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  runApp(ProviderScope(
+    overrides: [
+      sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+    ],
+    child: ShowsApp(),
+  ));
 }
 
 class ShowsApp extends StatelessWidget {
